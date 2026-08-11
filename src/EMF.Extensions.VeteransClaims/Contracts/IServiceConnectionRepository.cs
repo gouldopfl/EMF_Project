c051aea9ceeb6bb3ca1d4f401963a411f75b95e5
Michael Gould <gouldopfl@gmail.com>
2026-08-11 21:14:23 +0000

    Define service connection persistence contract

---
+++ src/EMF.Extensions.VeteransClaims/Contracts/IServiceConnectionRepository.cs
@@ -0,0 +1,40 @@
+using EMF.Extensions.VeteransClaims.Models.Identities;
+using EMF.Extensions.VeteransClaims.Models.Service;
+
+namespace EMF.Extensions.VeteransClaims.Contracts;
+
+public interface IServiceConnectionRepository
+{
+    Task AddServiceConnectionTheoryAsync(
+        ServiceConnectionTheory theory,
+        CancellationToken cancellationToken = default);
+
+    Task<ServiceConnectionTheory?>
+        GetServiceConnectionTheoryAsync(
+            ServiceConnectionTheoryId theoryId,
+            CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<ServiceConnectionTheory>>
+        GetServiceConnectionTheoriesAsync(
+            ClaimIssueId claimIssueId,
+            CancellationToken cancellationToken = default);
+    Task AddServiceConnectionBasisAsync(
+        ServiceConnectionBasis basis,
+        CancellationToken cancellationToken = default);
+
+    Task<ServiceConnectionBasis?>
+        GetServiceConnectionBasisAsync(
+            ServiceConnectionBasisId basisId,
+            CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<ServiceConnectionBasis>>
+        GetServiceConnectionBasesAsync(
+            ClaimIssueId claimIssueId,
+            CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<ServiceConnectionBasis>>
+        GetServiceConnectionBasesAsync(
+            ServiceConnectionTheoryId theoryId,
+            CancellationToken cancellationToken = default);
+
+}
