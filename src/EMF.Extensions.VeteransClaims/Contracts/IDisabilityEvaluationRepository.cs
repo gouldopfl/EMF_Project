55bcc2bd1a31bfc874fbc33068d89458497fc5d8
Michael Gould <gouldopfl@gmail.com>
2026-08-11 19:13:44 +0000

    Define disability evaluation repository contract

---
+++ src/EMF.Extensions.VeteransClaims/Contracts/IDisabilityEvaluationRepository.cs
@@ -0,0 +1,24 @@
+using EMF.Extensions.VeteransClaims.Models.Adjudication;
+using EMF.Extensions.VeteransClaims.Models.Identities;
+
+namespace EMF.Extensions.VeteransClaims.Contracts;
+
+public interface IDisabilityEvaluationRepository
+{
+    Task AddEvaluationsAsync(
+        IssueDecisionId issueDecisionId,
+        IReadOnlyCollection<DisabilityEvaluation>
+            evaluations,
+        IReadOnlyCollection<EffectiveDate>
+            effectiveDates,
+        CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<DisabilityEvaluation>>
+        GetEvaluationsAsync(
+            IssueDecisionId issueDecisionId,
+            CancellationToken cancellationToken = default);
+
+    Task<EffectiveDate?> GetEffectiveDateAsync(
+        DisabilityEvaluationId disabilityEvaluationId,
+        CancellationToken cancellationToken = default);
+}
