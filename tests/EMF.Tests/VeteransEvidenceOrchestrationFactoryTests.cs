b24e09ccd7437f9e9c22985339325c7d1fd4397a
Michael Gould <gouldopfl@gmail.com>
2026-08-20 21:14:12 +0000

    Test veterans evidence orchestration factory

---
+++ tests/EMF.Tests/VeteransEvidenceOrchestrationFactoryTests.cs
@@ -0,0 +1,44 @@
+using EMF.Extensions.VeteransClaims.Orchestration;
+using EMF.Intelligence.Capabilities;
+using EMF.Intelligence.Contracts;
+using EMF.Orchestration.Contracts;
+
+namespace EMF.Tests;
+
+public sealed class VeteransEvidenceOrchestrationFactoryTests
+{
+    [Fact]
+    public void CreateEvidenceSummaryPromotionService_RequiresPromotionService()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () =>
+                VeteransEvidenceOrchestrationFactory
+                    .CreateEvidenceSummaryPromotionService(null!));
+    }
+
+    [Fact]
+    public void CreateEvidenceDevelopmentWorkflowCoordinator_RequiresDependencies()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () =>
+                VeteransEvidenceOrchestrationFactory
+                    .CreateEvidenceDevelopmentWorkflowCoordinator(
+                        null!,
+                        null!,
+                        null!,
+                        null!,
+                        null!));
+    }
+
+    [Fact]
+    public void CreateEvidenceDevelopmentIntelligenceCoordinator_RequiresDependencies()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () =>
+                VeteransEvidenceOrchestrationFactory
+                    .CreateEvidenceDevelopmentIntelligenceCoordinator(
+                        null!,
+                        null!,
+                        null!));
+    }
+}
